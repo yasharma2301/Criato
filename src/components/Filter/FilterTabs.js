0cdd60bc927dea8483170ab74726b520ebbc1410
Yash Sharma <ys909@snu.edu.in>
2022-05-11 12:28:10 +0530

    fix modal css

--- src/components/Filter/FilterTabs.js
+++ src/components/Filter/FilterTabs.js
@@ -11,19 +11,19 @@ export default function FilterTabs({ closeModal }) {
     const [tabIndex, setTabIndex] = useState(0)
 
     return (
-        <div className='flex min-h-full'>
-            <div className='flex flex-col bg-zinc-100'>
+        <div className='flex min-h-full flex-col md:flex-row'>
+            <div className='flex flex-row md:flex-col bg-zinc-100'>
                 {
                     tabs.map((tab, idx) => {
                         return (
-                            <div key={`${tab.name}-Tab`} onClick={() => setTabIndex(idx)} className={`${idx === tabIndex ? 'border-red-400' : 'border-transparent'} border-l-8  p-5 hover:bg-zinc-200 cursor-pointer font-semibold tracking-wider text-lg`}>
+                            <div key={`${tab.name}-Tab`} onClick={() => setTabIndex(idx)} className={`${idx === tabIndex ? 'border-red-400' : 'border-transparent'} md:border-l-8 border-b-8 md:border-b-0 p-5 hover:bg-zinc-200 cursor-pointer font-semibold tracking-wider text-lg `}>
                                 {tab.name}
                             </div>
                         )
                     })
                 }
             </div>
-            <div className='w-full p-5'>
+            <div className='w-full p-5 flex items-center justify-center'>
                 {
                     tabIndex === 0 ? <SortBy /> : <CuisineFilter />
                 }
